d69e221e8b48416d3d2aa7e99261ab1dc3fdc21e
DestoryG <136814177+DestoryG@users.noreply.github.com>
2025-01-20 09:55:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,14 +16,4 @@ sed -i 's/KERNEL_PATCHVER:=5\.4/KERNEL_PATCHVER:=5.10/' target/linux/ramips/Make
 sed -i "s/set system.@system\[-1\].hostname='OpenWrt'/set system.@system[-1].hostname='DGWrt'/" package/base-files/files/bin/config_generate
 #mv 322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
 mv files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/
-rm -rf package/feeds/routing/batman-adv
-mv files/batman-adv package/feeds/routing
-rm feeds/packages/lang/lua-eco/Makefile
-wget https://raw.githubusercontent.com/zhaojh329/lua-eco/openwrt-package/Makefile -O feeds/packages/lang/lua-eco/Makefile
-rm package/feeds/packages/rust/Makefile
-mv files/rustfix/Makefile package/feeds/packages/rust
-git clone https://github.com/sbwml/luci-app-alist package/alist
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git packages
 sudo apt install libfuse-dev
-rm -rf feeds/packages/lang/node
-git clone https://github.com/sbwml/feeds_packages_lang_node-prebuilt -b packages-24.10 feeds/packages/lang/node
