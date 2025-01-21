b7c28fcbc18da6d5cdd989b3f719de86d5f2cbcf
DestoryG <136814177+DestoryG@users.noreply.github.com>
2025-01-22 00:02:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,6 @@ sed -i 's/KERNEL_PATCHVER:=5\.4/KERNEL_PATCHVER:=5.10/' target/linux/ramips/Make
 sed -i "s/set system.@system\[-1\].hostname='OpenWrt'/set system.@system[-1].hostname='DGWrt'/" package/base-files/files/bin/config_generate
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git packages
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i '/^PKG_BUILD_PARALLEL:=1$/a PKG_USE_MIPS16:=0' feeds/packages/utils/v2dat/Makefile
 rm -rf feeds/routing/batman-adv
 mv files/batman-adv feeds/routing
